23e028fe1be884c0e66f22f8d6492d04f7656030
gitandlucsil <andlucsil1221@gmail.com>
2021-12-29 10:16:20 -0300

    created slide images example

--- 5-jquery/script_query.js
+++ 5-jquery/script_query.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
     $('#azul').click(function(){
-        $('p').css("color", "blue");
-        $('p').fadeOut('fast');
-        $('p').delay(1000);
-        $('p').fadeIn('slow');
+        $('p')
+            .css("color", "blue")
+            .fadeOut('fast')
+            .delay(1000)
+            .fadeIn('slow')
+            .addClass('green');
     });
     $('#vermelho').click(function(){
-        $('p').css("color", "red");
-        $('p').fadeOut('slow');
-        $('p').delay(1000);
-        $('p').fadeIn('fast');
+        $('p')
+            .css("color", "red")
+            .fadeOut('slow')
+            .delay(1000)
+            .fadeIn('fast');
+        $('#mensagem')
+            .css({color:'red', border:'1px solid red', backgroundColor:'#F08080'})
+            .text("Cor alterada com sucesso")
+            .fadeOut(3000)
+        $('button').removeClass('red');
+    });
+    $('#l1').click(function(){
+        $('#i1').show();
+        $('#i2').hide();
+    });
+    $('#l2').click(function(){
+        $('#i1').hide();
+        $('#i2').show();
     });
 });
